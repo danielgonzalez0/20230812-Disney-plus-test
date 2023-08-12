e688081518db97f9a84470fe3689d40032ac04c5
Daniel Gonzalez <daniel.gonzalez.02@gmail.com>
2023-08-12 10:40:17 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css'
+
+const App = () => {
+  return (
+    <div className='app'>
+      <h2>C'est parti pour cloner Disney plus en react</h2>
+    </div>
+  );
+};
+
+export default App;
